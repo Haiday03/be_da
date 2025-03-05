6027150dc3c2a5aef15cbe10eae6859221e6d4c4
van hai <vanhai200345@gmail.com>
2025-03-05 22:41:20 +0700

    Fix loi nullpointer

--- src/main/java/com/lms/service/BookService.java
+++ src/main/java/com/lms/service/BookService.java
@@ -26,3 +26,62 @@ public interface BookService {
 	public BookForDashboard[] getRecentSales();
 
 }
+
+package com.lms.service;
+
+		import java.util.List;
+		import java.util.Optional;
+
+		import com.lms.dto.*;
+		import com.lms.dto.exception.NotFoundException;
+
+		import com.lms.model.Book;
+
+
+public interface BookService {
+	public BookOneDto save(BookOneDto bookOneDto) throws Exception;
+	public List<BookDto> getAll(String code, String name) throws NotFoundException;
+	public BookDto update(Long id, BookDto bookDto) throws NotFoundException;
+	public Optional<BookOneDto> getOne(Long id) throws NotFoundException;
+
+	public Optional<Book> getById(Long id) throws NotFoundException;
+	public Boolean delete(Long id) throws NotFoundException;
+	Boolean deleteList(BookDto[] dtos) throws NotFoundException;
+	public List<BookDto> SearchBooksByName(String name) throws NotFoundException;
+
+	public void Slope();
+
+	public BookRecDto[] getRecommended(BookOneDto bookOneDto);
+	public BookForDashboard[] getRecentSales();
+
+}
+
+
+package com.lms.service;
+
+		import java.util.List;
+		import java.util.Optional;
+
+		import com.lms.dto.*;
+		import com.lms.dto.exception.NotFoundException;
+
+		import com.lms.model.Book;
+
+
+public interface BookService {
+	public BookOneDto save(BookOneDto bookOneDto) throws Exception;
+	public List<BookDto> getAll(String code, String name) throws NotFoundException;
+	public BookDto update(Long id, BookDto bookDto) throws NotFoundException;
+	public Optional<BookOneDto> getOne(Long id) throws NotFoundException;
+
+	public Optional<Book> getById(Long id) throws NotFoundException;
+	public Boolean delete(Long id) throws NotFoundException;
+	Boolean deleteList(BookDto[] dtos) throws NotFoundException;
+	public List<BookDto> SearchBooksByName(String name) throws NotFoundException;
+
+	public void Slope();
+
+	public BookRecDto[] getRecommended(BookOneDto bookOneDto);
+	public BookForDashboard[] getRecentSales();
+
+}
--- src/main/java/com/lms/service/BorrowService.java
+++ src/main/java/com/lms/service/BorrowService.java
@@ -13,3 +13,36 @@ public interface BorrowService {
 	BorrowDto getOne(Long id) throws NotFoundException;
 	Boolean delete(Long id) throws NotFoundException;
 }
+
+package com.lms.service;
+
+		import com.lms.dto.BorrowDto;
+		import com.lms.dto.exception.NotFoundException;
+
+		import javax.validation.Valid;
+		import java.util.List;
+
+public interface BorrowService {
+	BorrowDto save(BorrowDto borrowDto);
+	List<BorrowDto> getAll() throws NotFoundException;
+	BorrowDto update(Long id, @Valid BorrowDto borrowDto) throws NotFoundException;
+	BorrowDto getOne(Long id) throws NotFoundException;
+	Boolean delete(Long id) throws NotFoundException;
+}
+
+
+package com.lms.service;
+
+		import com.lms.dto.BorrowDto;
+		import com.lms.dto.exception.NotFoundException;
+
+		import javax.validation.Valid;
+		import java.util.List;
+
+public interface BorrowService {
+	BorrowDto save(BorrowDto borrowDto);
+	List<BorrowDto> getAll() throws NotFoundException;
+	BorrowDto update(Long id, @Valid BorrowDto borrowDto) throws NotFoundException;
+	BorrowDto getOne(Long id) throws NotFoundException;
+	Boolean delete(Long id) throws NotFoundException;
+}
