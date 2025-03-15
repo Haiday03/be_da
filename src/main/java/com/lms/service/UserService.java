a9b28888fcacaa4823f03a74bb6282d69b32705d
van hai <vanhai200345@gmail.com>
2025-03-15 09:19:35 +0700

    Fix thong tin ng dung

--- src/main/java/com/lms/service/UserService.java
+++ src/main/java/com/lms/service/UserService.java
@@ -21,3 +21,52 @@ public interface UserService {
 
 
 }
+
+package com.lms.service;
+
+		import java.util.List;
+
+		import javax.validation.Valid;
+
+		import com.lms.dto.UserDashboard;
+		import com.lms.dto.UserDto;
+		import com.lms.dto.exception.NotFoundException;
+
+
+public interface UserService {
+
+	public UserDto save(@Valid UserDto UserDto) throws Exception;
+	public List<UserDto> getAll() throws NotFoundException;
+	public UserDto update(Long id, @Valid UserDto UserDto) throws Exception;
+	public Boolean delete(String username) throws NotFoundException;
+	public UserDashboard getUsersCount()  throws NotFoundException;
+	public List<Integer> UsersMonthly()  throws NotFoundException;
+
+
+
+}
+
+
+package com.lms.service;
+
+		import java.util.List;
+
+		import javax.validation.Valid;
+
+		import com.lms.dto.UserDashboard;
+		import com.lms.dto.UserDto;
+		import com.lms.dto.exception.NotFoundException;
+
+
+public interface UserService {
+
+	public UserDto save(@Valid UserDto UserDto) throws Exception;
+	public List<UserDto> getAll() throws NotFoundException;
+	public UserDto update(Long id, @Valid UserDto UserDto) throws Exception;
+	public Boolean delete(String username) throws NotFoundException;
+	public UserDashboard getUsersCount()  throws NotFoundException;
+	public List<Integer> UsersMonthly()  throws NotFoundException;
+
+
+
+}
